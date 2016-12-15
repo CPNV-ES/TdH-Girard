442c210a4023bd538de996b3fca98d9795685768
Quentin Girard <quentin.girard@cpnv.ch>
2016-12-15 12:10:21 +0100

    Add pse form

--- TdH-2/Models/Metadata.cs
+++ TdH-2/Models/Metadata.cs
@@ -241,5 +241,170 @@ namespace TdH_2.Models
         [Display(Name = "Résumé de l'incident (MAX 100 MOTS, respecter l’anonymat des intervenants)")]
         [DataType(DataType.MultilineText)]
         public string resume_incident;
+
+        [Display(Name = "Violence physique")]
+        public string violence_physique;
+
+        [Display(Name = "Violence sexuelle")]
+        public string violence_sexuelle;
+
+        [Display(Name = "Violence psychologique")]
+        public string violence_psychologique;
+
+        [Display(Name = "Négligence")]
+        public string negligence;
+
+        [Display(Name = "Détails de l'incident")]
+        public string details_de_lincident;
+
+        [Display(Name = "Status")]
+        public string victime_enfant_statut;
+
+        [Display(Name = "Nom")]
+        public string victime_enfant_nom;
+
+        [Display(Name = "Date de naissance")]
+        public string victime_enfant_date_de_naissance;
+
+        [Display(Name = "Age")]
+        public string victime_enfant_age;
+
+        [Display(Name = "Sexe")]
+        public string victime_enfant_sexe;
+
+        [Display(Name = "Adresse")]
+        public string victime_enfant_adresse;
+
+        [Display(Name = "Handicap - si applicable (physique ou mental)")]
+        public string victime_enfant_handicap;
+
+        [Display(Name = "Autre information importante")]
+        public string victime_enfant_autre_information_importante;
+
+        [Display(Name = "Status")]
+        public string agresseur_statut;
+
+        [Display(Name = "Nom")]
+        public string agresseur_nom;
+
+        [Display(Name = "Position")]
+        public string agresseur_position;
+
+        [Display(Name = "Age")]
+        public string agresseur_age;
+
+        [Display(Name = "Sexe")]
+        public string agresseur_sexe;
+
+        [Display(Name = "Statut familial")]
+        public string agresseur_statut_familial;
+
+        [Display(Name = "Nom de l'employeur")]
+        public string agresseur_nom_de_lemployeur;
+
+        [Display(Name = "Lien avec TdH")]
+        public string agresseur_lien_avec_tdh;
+
+        [Display(Name = "Lien avec l'enfant")]
+        public string agresseur_lien_avec_lenfant;
+
+        [Display(Name = "Autre information importante")]
+        public string agresseur_autre_information_importante;
+
+        [Display(Name = "Nom")]
+        public string personne_signalant_nom;
+
+        [Display(Name = "Position")]
+        public string personne_signalant_position;
+
+        [Display(Name = "Adresse")]
+        public string personne_signalant_adresse;
+
+        [Display(Name = "Nom de l'employeur")]
+        public string personne_signalant_nom_de_lemployeur;
+
+        [Display(Name = "Lien avec TdH")]
+        public string personne_signalant_lien_avec_tdh;
+
+        [Display(Name = "Lien avec l'enfant")]
+        public string personne_signalant_lien_avec_lenfant;
+
+        [Display(Name = "Autre information importante")]
+        public string personne_signalant_autre_information_importante;
+
+        [Display(Name = "Pour protéger l'enfant")]
+        public string mesures_prises_pour_proteger_lenfant;
+
+        [Display(Name = "Pour éviter la récidive de l’auteur présumé")]
+        public string mesures_prises_pour_eviter_la_recidive;
+
+        [Display(Name = "Pour protéger l’action de la fondation Terre des hommes")]
+        public string mesures_prises_pour_proteger_laction_de_tdh;
+
+        [Display(Name = "Qu’est-ce que l’enfant souhaite ? / Quelle est sa vision de la situation ?")]
+        public string informations_supplementaires_quest_ce_que_lenfant_souhaite;
+
+        [Display(Name = "De quel soutien l’enfant a-t-il besoin ?   (tel que médical & autre)  ")]
+        public string informations_supplementaires_de_quel_soutien_lenfant_a_t_il_besoin;
+
+        [Display(Name = "Qui doit apporter ce soutien et sous quel délai ?")]
+        public string informations_supplementaires_qui_doit_apporter_ce_soutien;
+
+        [Display(Name = "Y-a-t-il d’autres actions à prendre ?")]
+        public string informations_supplementaires_y_a_t_il_dautres_actions;
+
+        [Display(Name = "Préciser par qui & quand")]
+        public string informations_supplementaires_qui_quand;
+
+        [Display(Name = "Qui va suivre ce cas ?")]
+        public string informations_supplementaires_qui_va_suivre;
+
+        [Display(Name = "Y-a-t-il d’autres leçons apprises à la suite de cet incident qui pourraient être utiles pour le futur ? Dans l’affirmative, quelles sont les prochaines étapes à mettre en place pour éviter qu’un tel incident ne se reproduise ?")]
+        public string informations_supplementaires_lecons_apprises;
+
+        [Display(Name = "Recommandations du délégué et/ou du point focal de la sauvegarde de l’enfant")]
+        public string recommandations_du_delegue;
+
+        [Display(Name = "Rapport rempli par (Nom et Position)")]
+        public string rapport_rempli_par;
+
+        [Display(Name = "Date de l’envoi au siège:")]
+        public string date_de_lenvoi_au_siege;
+
+        [Display(Name = "Rapport traité par :(au niveau du terrain)")]
+        public string rapport_traite_par;
+
+        [Display(Name = "Mise à jour du cas")]
+        public string mise_a_jour_du_cas;
+
+        [Display(Name = "Date")]
+        public string mise_a_jour_du_cas_date;
+
+        [Display(Name = "Numéro de l'incident")]
+        public string a_remplir_par_le_siege_numero_de_lincident;
+
+        [Display(Name = "Reçu par")]
+        public string a_remplir_par_le_siege_recu_par;
+
+        [Display(Name = "Autre")]
+        public string a_remplir_par_le_siege_recu_par_autre;
+
+        [Display(Name = "TRaité au siège par")]
+        public string a_remplir_par_le_siege_traite_au_siege_par;
+
+        [Display(Name = "Mise à jour / Recommandation")]
+        public string a_remplir_par_le_siege_mise_a_jour_recommandation;
+
+        [Display(Name = "Date de fermeture")]
+        public string a_remplir_par_le_siege_date_de_fermeture;
+
+        [Display(Name = "Statut")]
+        public string a_remplir_par_le_siege_statut;
+
+        [Display(Name = "Année")]
+        public string annee;
+
+        [Display(Name = "Langue")]
+        public string langue;
     }
 }
